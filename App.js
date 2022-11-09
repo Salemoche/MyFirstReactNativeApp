a2a6197eb04a264ab8b3b3b0caa27b59cf1eabb0
Gabriel Bach <gabrielmolembi@hotmail.com>
2022-11-09 13:58:00 +0100

    initial

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, Pressable } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    const [count, setCount] = useState(0);
+
+    return (
+        <View style={styles.container}>
+            <Pressable onPress={ () => setCount( count + 1 ) } onLongPress={ () => setCount( count + 10 ) } style={[ styles.pressable, styles.topLeft ]}></Pressable>
+            <Pressable onPress={ () => setCount( count + 1 ) } onLongPress={ () => setCount( count + 10 ) } style={[ styles.pressable, styles.topRight ]}></Pressable>
+            <Pressable onPress={ () => setCount( count + 1 ) } onLongPress={ () => setCount( count + 10 ) } style={[ styles.pressable, styles.bottomLeft ]}></Pressable>
+            <Pressable onPress={ () => setCount( count + 1 ) } onLongPress={ () => setCount( count + 10 ) } style={[ styles.pressable, styles.bottomRight ]}></Pressable>
+            <View pointerEvents='none' style={ styles.counter }>
+                <Text style={{ fontSize: 30 }}>{ count }</Text>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'flex-start',
+        justifyContent: 'flex-start',
+        flexDirection: 'row',
+        flexWrap: 'wrap'
+    },
+    counter: {
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+        color: 'blue',
+        justifyContent: 'center',
+        alignItems: 'center',
+        zIndex: '100',
+    },
+    pressable: {
+        flexBasis: '50%',
+        height: '50%'
+    },
+    topLeft: {
+        backgroundColor: '#ff0',
+    },
+    topRight: {
+        backgroundColor: '#f0f',
+    },
+    bottomLeft: {
+        backgroundColor: '#0ff',
+    },
+    bottomRight: {
+        backgroundColor: '#fff',
+    },
 });
